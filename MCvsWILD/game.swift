a8d97c71dacefeb496cfca7dfbec4f62c6989b04
Jonathan & Valentin <jonathan0780@hotmail.fr>
2021-01-05 17:22:13 +0100

    First commit

---
+++ MCvsWILD/game.swift
@@ -0,0 +1,17 @@
+//
+//  game.swift
+//  MCvsWILD
+//
+//  Created by “ios” on 05/01/2021.
+//
+
+import Foundation
+import UIKit
+
+class Game: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ MCvsWILD/statBar.swift
@@ -0,0 +1,21 @@
+//
+//  statBar.swift
+//  MCvsWILD
+//
+//  Created by “ios” on 05/01/2021.
+//
+
+import Foundation
+import UIKit
+
+class StatBar: UIProgressView{
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        let maskLayerPath = UIBezierPath(roundedRect: bounds, cornerRadius: 4.0)
+        let maskLayer = CAShapeLayer()
+        maskLayer.frame = self.bounds
+        maskLayer.path = maskLayerPath.cgPath
+        layer.mask = maskLayer
+    }
+}
