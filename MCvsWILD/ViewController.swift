12ecb5a68bf8941131428c9afe593fe1c6c4853e
Jonathan & Valentin <jonathan0780@hotmail.fr>
2021-01-05 17:24:43 +0100

    First Commit

---
+++ MCvsWILD/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MCvsWILD
+//
+//  Created by “ios” on 05/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var play_btn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MCvsWILDTests/MCvsWILDTests.swift
@@ -0,0 +1,33 @@
+//
+//  MCvsWILDTests.swift
+//  MCvsWILDTests
+//
+//  Created by “ios” on 05/01/2021.
+//
+
+import XCTest
+@testable import MCvsWILD
+
+class MCvsWILDTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
